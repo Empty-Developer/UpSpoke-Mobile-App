4445bd13a11de2f6aebd48c87f8f0bc2a62e527f
Empty <emptypageworking@gmail.com>
2026-04-11 06:25:53 +0700

    readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # UpSpoke
 
+[![wakatime](https://wakatime.com/badge/user/fdcea843-56ea-404c-838b-f7f306de46b6/project/8ac0f7e0-c130-486b-96a5-44b26f30c9c6.svg)](https://wakatime.com/badge/user/fdcea843-56ea-404c-838b-f7f306de46b6/project/8ac0f7e0-c130-486b-96a5-44b26f30c9c6)
+
 <p align="center">
   <img src="/logo.png" alt="logo" width="200">
 </p>
--- learning-app/app/(tabs)/_layout.tsx
+++ learning-app/app/(tabs)/_layout.tsx
@@ -1,13 +1,7 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 import { View } from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <View>
